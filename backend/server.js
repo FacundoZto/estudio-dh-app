b7f4fa994e45cadfaeef3a15830365fbd8ebf2c3
facundozto <facundozerbato@outlook.com>
2025-04-10 14:50:10 -0300

    update port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/api', mailRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
